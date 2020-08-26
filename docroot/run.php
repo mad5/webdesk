e3273928db029442d97c8ad97ca1edeeb5c1891b
Aresch Yavari <github@flipflox.de>
2020-08-26 08:08:58 +0200

    first commit

---
+++ docroot/run.php
@@ -0,0 +1,16 @@
+<!doctype HTML>
+<html>
+<head>
+<title>webdeskwindow</title>
+<style>
+body {
+    background: url(bg.jpg) no-repeat center center fixed;
+    background-size: cover;
+}
+</style>
+</head>
+<body bgcolor="black">
+
+Test
+</body>
+</html>
